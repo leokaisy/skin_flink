ba50cb9c7ccd4f8d96ae64af492067c91d7fb936
kaisy <18170070825@163.com>
2022-05-07 17:36:40 +0800

    flink transformation learning

---
+++ src/main/java/com/program/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.program.git;
+
+/**
+ * @author : kaisy
+ * @date : 2022/5/7 17:32
+ * @Description : test
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("git ok");
+    }
+}
