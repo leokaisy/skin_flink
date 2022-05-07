a5dde6f87a35d96092c6414cb1b9194ca6224325
kaisy <18170070825@163.com>
2022-05-07 17:43:53 +0800

    git change

--- src/main/java/com/program/git/GitTest.java
+++ src/main/java/com/program/git/GitTest.java
@@ -8,5 +8,6 @@ package com.program.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("git ok");
+        System.out.println("private");
     }
 }
